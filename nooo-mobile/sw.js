4290aaf8d2ccadf3d93d87e3a38ac5ce147e1ae1
headrock10-prog <headrock10@gmail.com>
2026-07-11 09:27:03 +0900

    Add files via upload

---
+++ nooo-mobile/sw.js
@@ -0,0 +1,32 @@
+const CACHE='nooo-mobile-v1';
+const FILES=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg',
+  './assets/gothic-knight-sprites.png',
+  './assets/gothic-monsters-atlas.png',
+  './assets/gothic-bosses-atlas.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==self.location.origin)return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
